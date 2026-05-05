a68bfbc5aef9b75df3eeada76e02cfc3d29f50b8
krzychu <krzysztof.nowak2005@gmail.com>
2026-05-05 08:48:34 +0200

    adding functionality to store multiple buildings

--- src/main/java/pl/put/poznan/buildinginfo/rest/BuildingInfoController.java
+++ src/main/java/pl/put/poznan/buildinginfo/rest/BuildingInfoController.java
@@ -1,9 +1,14 @@
 package pl.put.poznan.buildinginfo.rest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import pl.put.poznan.buildinginfo.logic.AreaVisitor;
 import pl.put.poznan.buildinginfo.logic.Building;
+
+import java.awt.geom.Area;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 
 @RestController
@@ -11,27 +16,37 @@ import pl.put.poznan.buildinginfo.logic.Building;
 public class BuildingInfoController {
 
     private static final Logger logger = LoggerFactory.getLogger(BuildingInfoController.class);
-    private AreaVisitor areaVisitor;
+    private final List<Building> buildingsCollection = new CopyOnWriteArrayList<>();
 
     @RequestMapping(value = "/post", method = RequestMethod.POST, produces = "application/json")
-    public void sendArea(@RequestBody Building building) {
+    public void sendBuilding(@RequestBody Building building) {
 
-        // log the parameters
-        logger.debug("Sending building: " + building.getName());
-
-        // perform the transformation, you should run your logic here, below is just a silly example
-        this.areaVisitor = new AreaVisitor();
-        building.accept(areaVisitor);
-
+        logger.debug("Adding building to collection: " + building.getName()+ " with id "+ building.getId());
+        buildingsCollection.add(building);
         logger.debug("Building send: " + building.getName());
     }
 
-    @RequestMapping(value = "/area", method = RequestMethod.GET, produces = "application/json")
-    public long getArea(@RequestBody Building building) {
+    @RequestMapping(value = "/area/{targetId}", method = RequestMethod.GET, produces = "application/json")
+    public long getArea(@PathVariable int targetId) {
 
-        // log the parameters
-        logger.debug("Calculating Area for: " + building.getName());
-        logger.debug("Area of: " + building.getName() + ": " + areaVisitor.getResult());
+        logger.debug("Looking for building with ID " + targetId);
+        Building targetBuilding = null;
+        for (Building building : buildingsCollection) {
+            if (building.getId() == targetId) {
+                targetBuilding = building;
+                logger.debug("Building with id " + targetId + " found.");
+                break;
+            }
+        }
+        if (targetBuilding == null) {
+            logger.debug("Building with id " + targetId + " not found.");
+            return ResponseEntity.notFound().build().getStatusCodeValue();
+        }
+
+        logger.debug("Creating Area Visitor and calculating area for building");
+        AreaVisitor areaVisitor = new AreaVisitor();
+        targetBuilding.accept(areaVisitor);
+        logger.debug("Area of: " + targetBuilding.getName() + ": " + areaVisitor.getResult());
 
         return areaVisitor.getResult();
     }
