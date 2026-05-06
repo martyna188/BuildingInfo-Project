ea2189813ac60e8dfe6aec10d45d0ed119eb649d
krzychu <krzysztof.nowak2005@gmail.com>
2026-05-06 14:02:04 +0200

    description of the interfaces for JavaDoc

--- src/main/java/pl/put/poznan/buildinginfo/logic/Location.java
+++ src/main/java/pl/put/poznan/buildinginfo/logic/Location.java
@@ -1,7 +1,24 @@
 package pl.put.poznan.buildinginfo.logic;
 
+/**
+ * Represents a physical node that can be processed by the concrete Visitor
+ * <p>
+ * This interface defines Element role in the Visitor pattern, providing easy way for algorithms
+ * to traverse data without modifying concrete object
+ */
 public interface Location {
+    /**
+     * @return the unique ID characteristic for each Location
+     */
     public int getId();
+    /**
+     * @return human-readable name of the given Location
+     */
     public String getName();
+
+    /**
+     * Accepts Visitor that perform specific operation on this Location
+     * @param v the Visitor that will perform operation on this Location
+     */
     public void accept(Visitor v);
 }
--- src/main/java/pl/put/poznan/buildinginfo/logic/Visitor.java
+++ src/main/java/pl/put/poznan/buildinginfo/logic/Visitor.java
@@ -1,6 +1,22 @@
 package pl.put.poznan.buildinginfo.logic;
 
+/**
+ * Defines an operation to be performed on the elements of Room.
+ * <p>
+ * The Visitor pattern allows for adding new operations on Room without modifying it
+ */
 public interface Visitor {
+
+    /**
+     * Performs the visitor's specific logic on Room instance
+     * @param r the Room object (must not be null)
+     */
+
     public void visitRoom(Room r);
+
+    /**
+     * Retrieves final calculated result across all triggered objects
+     * @return a long representing calculated metrics
+     */
     public long getResult();
 }
