f42ec90ceff3a0f19c18dbbb1f1d641347c4af39
martyna188 <martyna.wagner@icloud.com>
2026-05-08 15:51:13 +0200

    Changed Location from interface to abstract class, added fields and methods for ID and name. Cleaned the inheriting classes Room, Level, Building. Postman is working.

--- src/main/java/pl/put/poznan/buildinginfo/logic/Composite/Building.java
+++ src/main/java/pl/put/poznan/buildinginfo/logic/Composite/Building.java
@@ -4,42 +4,14 @@ import pl.put.poznan.buildinginfo.logic.Visitor.Visitor;
 
 import java.util.ArrayList;
 
-public class Building implements Location {
+public class Building extends Location {
 
     public Building() {}
 
-    private int id;
-    private String name;
     /**
      * Stores levels that the building is composed of.
      */
     private ArrayList<Level> childrenLevels = new ArrayList<>();
-
-    /**
-     * Sets Id for the building, used for automatic creation from JSON
-     * @param id id
-     */
-    public void setId(int id) { this.id = id; }
-
-    /**
-     * Sets name for the building, used for automatic creation from JSON
-     * @param name name
-     */
-    public void setName(String name) { this.name = name; }
-
-    /**
-     * Returns ID of the building
-     * @return ID
-     */
-    @Override
-    public int getId(){return this.id;}
-
-    /**
-     * Returns name of the given building
-     * @return name
-     */
-    @Override
-    public String getName(){return this.name;}
 
     /**
      * Sets for the building the levels that it is composed of
--- src/main/java/pl/put/poznan/buildinginfo/logic/Composite/Room.java
+++ src/main/java/pl/put/poznan/buildinginfo/logic/Composite/Room.java
@@ -2,20 +2,12 @@ package pl.put.poznan.buildinginfo.logic.Composite;
 
 import pl.put.poznan.buildinginfo.logic.Visitor.Visitor;
 
-public class Room implements Location {
-    private int id;
-    private String name;
+public class Room extends Location {
+
     private long area;
     private long cubature;
     private long light;
     private long heating;
-
-
-    @Override
-    public int getId() {return this.id;}
-
-    @Override
-    public String getName() {return this.name;}
 
     @Override
     public void accept(Visitor v) {
@@ -35,8 +27,6 @@ public class Room implements Location {
         return this.heating;
     }
 
-    public void setId(int id) { this.id = id; }
-    public void setName(String name) { this.name = name; }
     public void setArea(long area) { this.area = area; }
     public void setCubature(long cubature) { this.cubature = cubature; }
     public void setLight(long light) { this.light = light; }
