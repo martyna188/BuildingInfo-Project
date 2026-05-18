8d04854a8b41f195d0cac3d9d97a64b2045cf20b
krzychu <krzysztof.nowak2005@gmail.com>
2026-05-18 20:20:07 +0200

    adding authors and releases for project for javadoc

--- src/main/java/pl/put/poznan/buildinginfo/app/BuildingInfoApplication.java
+++ src/main/java/pl/put/poznan/buildinginfo/app/BuildingInfoApplication.java
@@ -3,7 +3,13 @@ package pl.put.poznan.buildinginfo.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
+/**
+ * Main application entry point for the Building Info REST API.
+ * @author Krzysztof
+ * @author Martyna
+ * @author Damian
+ * @version 1.0.1
+ */
 @SpringBootApplication(scanBasePackages = {"pl.put.poznan.buildinginfo.rest"})
 public class BuildingInfoApplication {
 
